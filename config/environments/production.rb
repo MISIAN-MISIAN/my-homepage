d23e12cd5db68f38af2ec7b92e0844b183c7293d
MISIAN-MISIAN <baikinman0907kitanai@icloud.com>
2020-01-03 12:04:32 +0900

    修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,8 +23,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-
+  config.assets.compile = true
+  config.assets.initialize_on_precompile=false
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
@@ -84,7 +84,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  
-  # herokuでデプロイした時にj書見込むようにする
-  config.assets.compile = true
+
+
 end
