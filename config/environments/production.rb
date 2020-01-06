c05cbaecddad4384d18bf0a30631c5052856cd99
MISIAN-MISIAN <baikinman0907kitanai@icloud.com>
2020-01-06 16:14:25 +0900

    修正
    修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
-    domain: "herokuapp.com",
+    domain: "mighty-fortress-25311.herokuapp.com",
     address: "smtp.sendgrid.net",
     port: 587,
     authentication: :plain,
