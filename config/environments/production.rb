3c2f85de7e56f7be94f6666dca583f3545382bf4
MISIAN-MISIAN <baikinman0907kitanai@icloud.com>
2020-01-09 19:17:25 +0900

    コメントアウト

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,8 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
-    domain: "icloud.com",
+    domain: "heroku.com",
+    # domain: "icloud.com"
     address: "smtp.sendgrid.net",
     port: 587,
     authentication: :plain,
