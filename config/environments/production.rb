0d6f684197de4eadb68d2d3550039b00c4f059d4
MISIAN-MISIAN <baikinman0907kitanai@icloud.com>
2021-06-06 17:14:01 +0900

    recommit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,6 +26,8 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
 
+  config.assets.debug = false
+
   # config.public_file_server.enabled = true # rails5〜
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
